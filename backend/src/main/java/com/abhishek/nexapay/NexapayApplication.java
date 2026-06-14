04f5d1855120f45dfced34abd9b626ab49ed4c69
Abhishek Gupta <abhishek.work07@gmail.com>
2026-06-14 23:27:39 +0530

    Initialized the NexaPay Backend project

---
+++ backend/src/main/java/com/abhishek/nexapay/NexapayApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.nexapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexapayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexapayApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/abhishek/nexapay/NexapayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.nexapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexapayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
